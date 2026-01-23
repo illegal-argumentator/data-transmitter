5f9429ea147a4bb7693512d2f744d95966a937c7
HaaaamBurger <gemasclashes@gmail.com>
2026-01-23 11:39:13 +0200

    init

---
+++ src/main/java/com/iglin/data_transmitter/DataTransmitterApplication.java
@@ -0,0 +1,13 @@
+package com.iglin.data_transmitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataTransmitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataTransmitterApplication.class, args);
+	}
+
+}
