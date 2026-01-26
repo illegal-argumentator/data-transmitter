4ab8d3338a5078a5a640c36e08c9f868ea346f24
HaaaamBurger <gemasclashes@gmail.com>
2026-01-26 12:14:12 +0200

    docker

--- src/main/java/com/iglin/data_transmitter/adapter/in/chatbase/ChatbaseWebhookController.java
+++ src/main/java/com/iglin/data_transmitter/adapter/in/chatbase/ChatbaseWebhookController.java
@@ -2,11 +2,13 @@ package com.iglin.data_transmitter.adapter.in.chatbase;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/webhook")
@@ -19,6 +21,7 @@ public class ChatbaseWebhookController {
             HttpServletRequest request,
             @RequestHeader("x-chatbase-signature") String signature
     ) {
+        log.info("Received request from chatbase webhook.");
         chatbaseWebhookHandler.handleFormSubmission(request, signature);
     }
 }
--- src/main/java/com/iglin/data_transmitter/adapter/in/chatbase/ChatbaseWebhookHandler.java
+++ src/main/java/com/iglin/data_transmitter/adapter/in/chatbase/ChatbaseWebhookHandler.java
@@ -4,8 +4,10 @@ import com.iglin.data_transmitter.adapter.in.HttpRequestParser;
 import com.iglin.data_transmitter.application.chatbase.ChatbaseService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ChatbaseWebhookHandler {
@@ -21,8 +23,9 @@ public class ChatbaseWebhookHandler {
     public void handleFormSubmission(HttpServletRequest request, String headerSignature) {
         String rawBody = httpRequestParser.parse(request);
         String parsedSignature = chatbaseParser.parseSignature(rawBody);
-        chatbaseSignatureValidator.validate(parsedSignature, headerSignature);
-        chatbaseService.save(chatbaseParser.parseConversation(rawBody));
+        log.info("Rawbody: {}, signature: {}", rawBody, parsedSignature);
+//        chatbaseSignatureValidator.validate(parsedSignature, headerSignature);
+//        chatbaseService.save(chatbaseParser.parseConversation(rawBody));
     }
 
 }
